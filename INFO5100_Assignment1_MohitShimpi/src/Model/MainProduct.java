4987bc3077d0f7c4e06b4590875c3860b92d39aa
MohitShimpi-NEU <shimpi.m@northeastern.edu>
2021-02-08 19:37:46 -0500

    first commit

---
+++ INFO5100_Assignment1_MohitShimpi/src/Model/MainProduct.java
@@ -0,0 +1,314 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author 16177
+ */
+public class MainProduct {
+    private String firstname;
+    private String lastname;
+    private String phonenumber;
+    private String dob;
+    private String age;
+    private String height;
+    private String weight;
+    private String ssn;
+    private String StreetAdrs;
+    private String City;
+    private String state;
+    private String zipcode;
+    private String Bankname;
+    private String bankroutingno;
+    private String bankaccno;
+    private String accbal;
+    private String acctype;
+    private String cBankname;
+    private String cbankroutingno;
+    private String cbankaccno;
+    private String caccbal;
+    private String cacctype;
+    private String licenseno;
+    private String doi;
+    private String doe;
+    private String bloodtype;
+    private String medrecno;
+    private String a1;
+    private String a2;
+    private String a3;
+     private String absolutepath;
+    public ImageIcon imageIcon;
+    
+
+    public ImageIcon getImageIcon() {
+        return imageIcon;
+    }
+
+    public void setImageIcon(ImageIcon imageIcon) {
+        this.imageIcon = imageIcon;
+    }
+
+    public String getAbsolutepath() {
+        return absolutepath;
+    }
+
+    public void setAbsolutepath(String absolutepath) {
+        this.absolutepath = absolutepath;
+    }
+
+    public String getMedrecno() {
+        return medrecno;
+    }
+
+    public void setMedrecno(String medrecno) {
+        this.medrecno = medrecno;
+    }
+
+    public String getA1() {
+        return a1;
+    }
+
+    public void setA1(String a1) {
+        this.a1 = a1;
+    }
+
+    public String getA2() {
+        return a2;
+    }
+
+    public void setA2(String a2) {
+        this.a2 = a2;
+    }
+
+    public String getA3() {
+        return a3;
+    }
+
+    public void setA3(String a3) {
+        this.a3 = a3;
+    }
+
+    
+    public String getLicenseno() {
+        return licenseno;
+    }
+
+    public void setLicenseno(String licenseno) {
+        this.licenseno = licenseno;
+    }
+
+    public String getDoi() {
+        return doi;
+    }
+
+    public void setDoi(String doi) {
+        this.doi = doi;
+    }
+
+    public String getDoe() {
+        return doe;
+    }
+
+    public void setDoe(String doe) {
+        this.doe = doe;
+    }
+
+    public String getBloodtype() {
+        return bloodtype;
+    }
+
+    public void setBloodtype(String bloodtype) {
+        this.bloodtype = bloodtype;
+    }
+    
+    
+
+    public String getcBankname() {
+        return cBankname;
+    }
+
+    public void setcBankname(String cBankname) {
+        this.cBankname = cBankname;
+    }
+
+    public String getCbankroutingno() {
+        return cbankroutingno;
+    }
+
+    public void setCbankroutingno(String cbankroutingno) {
+        this.cbankroutingno = cbankroutingno;
+    }
+
+    public String getCbankaccno() {
+        return cbankaccno;
+    }
+
+    public void setCbankaccno(String cbankaccno) {
+        this.cbankaccno = cbankaccno;
+    }
+
+    public String getCaccbal() {
+        return caccbal;
+    }
+
+    public void setCaccbal(String caccbal) {
+        this.caccbal = caccbal;
+    }
+
+    public String getCacctype() {
+        return cacctype;
+    }
+
+    public void setCacctype(String cacctype) {
+        this.cacctype = cacctype;
+    }
+
+
+    public String getBankname() {
+        return Bankname;
+    }
+
+    public void setBankname(String Bankname) {
+        this.Bankname = Bankname;
+    }
+
+    public String getBankroutingno() {
+        return bankroutingno;
+    }
+
+    public void setBankroutingno(String bankroutingno) {
+        this.bankroutingno = bankroutingno;
+    }
+
+    public String getBankaccno() {
+        return bankaccno;
+    }
+
+    public void setBankaccno(String bankaccno) {
+        this.bankaccno = bankaccno;
+    }
+
+    public String getAccbal() {
+        return accbal;
+    }
+
+    public void setAccbal(String accbal) {
+        this.accbal = accbal;
+    }
+
+    public String getAcctype() {
+        return acctype;
+    }
+
+    public void setAcctype(String acctype) {
+        this.acctype = acctype;
+    }
+    
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getStreetAdrs() {
+        return StreetAdrs;
+    }
+
+    public void setStreetAdrs(String StreetAdrs) {
+        this.StreetAdrs = StreetAdrs;
+    }
+    
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+    
+    
+    
+    
+}
