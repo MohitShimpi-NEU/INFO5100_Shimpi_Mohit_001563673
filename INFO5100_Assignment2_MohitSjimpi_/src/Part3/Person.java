1c5bee125608a1a6be96495ac0146819633225c0
MohitShimpi-NEU <shimpi.m@northeastern.edu>
2021-02-27 16:56:24 -0500

    First Commit
    Without Patient Normal

---
+++ INFO5100_Assignment2_MohitSjimpi_/src/Part3/Person.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Part3;
+
+/**
+ *
+ * @author 16177
+ */
+public class Person {
+    public int personId;
+    public String name;
+    public String age;
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+    
+ 
+}
